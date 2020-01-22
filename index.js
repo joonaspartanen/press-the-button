79a7c31c8fe9eb42307c785b5806884e11da30be
Joonas Partanen <partanen.joonas@gmail.com>
2020-01-22 18:38:44 +0200

    Refactor the backend code

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const app = require('./app')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+// Global counter
 let counter = 0
+
+// Clicks to the next prize
 let nextPrize = 10
 
+// Array to store the game state 
 let players = []
 
 const findPlayer = id => {
   return players.find(p => p.id === id)
+}
+
+const sendGameState = () => {
+  io.sockets.emit('gameState', {
+    players: players,
+    toNextPrize: nextPrize - counter
+  })
+}
+
+const handleWin = (prize, player) => {
+  nextPrize = nextPrize + 10
+  io.to(player.id).emit('win', prize)
+  return prize
 }
 
 io.on('connection', socket => {
@@ -16,10 +33,7 @@ io.on('connection', socket => {
 
   socket.on('newPlayer', name => {
     players.push({ name: name, id: socket.id, score: 20 })
-    io.sockets.emit('gameState', {
-      players: players,
-      toNextPrize: nextPrize - counter
-    })
+    sendGameState()
   })
 
   socket.on('click', () => {
@@ -27,62 +41,46 @@ io.on('connection', socket => {
     const player = findPlayer(socket.id)
     console.log(`player ${player.id} clicked the button`)
     console.log(`the counter is ${counter}`)
-    let newScore = -1
+
+    // Player loses 1 point if there's no prize
+    let scoreChange = -1
 
     if (counter % 500 === 0) {
-      newScore = newScore + 250
-      nextPrize = nextPrize + 10
-      io.to(player.id).emit('win', newScore + 1)
+      scoreChange = handleWin(250, player)
     } else if (counter % 100 === 0) {
-      newScore = newScore + 40
-      nextPrize = nextPrize + 10
-      io.to(player.id).emit('win', newScore + 1)
+      scoreChange = handleWin(40, player)
     } else if (counter % 10 === 0) {
-      newScore = newScore + 5
-      nextPrize = nextPrize + 10
-      io.to(player.id).emit('win', newScore + 1)
+      scoreChange = handleWin(5, player)
     } else {
       io.to(player.id).emit('noWin', nextPrize - counter)
       if (player.score === 1) {
-        console.log('you lose')
         io.to(player.id).emit('lostGame')
       }
     }
 
+    // Update the game state
     players = players.map(p =>
-      p.id !== player.id ? p : { ...p, score: p.score + newScore }
+      p.id !== player.id ? p : { ...p, score: p.score + scoreChange }
     )
 
     console.log(`${nextPrize - counter} clicks to the next prize`)
-    io.sockets.emit('gameState', {
-      players: players,
-      toNextPrize: nextPrize - counter
-    })
+    sendGameState()
   })
 
   socket.on('playAgain', () => {
     players = players.map(p => (p.id !== socket.id ? p : { ...p, score: 20 }))
-    io.sockets.emit('gameState', {
-      players: players,
-      toNextPrize: nextPrize - counter
-    })
+    sendGameState()
   })
 
   socket.on('leaveGame', () => {
     players = players.filter(p => p.id !== socket.id)
-    io.sockets.emit('gameState', {
-      players: players,
-      toNextPrize: nextPrize - counter
-    })
+    sendGameState()
   })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
     players = players.filter(p => p.id !== socket.id)
-    io.sockets.emit('gameState', {
-      players: players,
-      toNextPrize: nextPrize - counter
-    })
+    sendGameState()
   })
 })
 
