0630974187b6cff42e3bca941b64c0aa86464959
Joonas Partanen <partanen.joonas@gmail.com>
2020-01-28 19:14:17 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const path = require('path')
+process.env.PWD = process.cwd();
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(process.env.PWD, '../frontend/build')))
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '../frontend/index.html'))
+  response.sendFile(path.join(process.env.PWD, '../frontend/index.html'))
 })
 
 module.exports = app
