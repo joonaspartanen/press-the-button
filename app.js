46b20e1ac152ce336001d98f6ce83a4793c9e4ae
Joonas Partanen <partanen.joonas@gmail.com>
2020-01-28 19:02:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const cors = require('cors')
 const path = require('path')
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, '/../frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '../frontend/index.html'))
 })
 
 module.exports = app
