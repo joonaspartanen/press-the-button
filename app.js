31e418ade3c505b6ff02cf75bd3b6b05f666e668
Joonas Partanen <partanen.joonas@gmail.com>
2020-01-28 18:05:41 +0200

    Add endpoint

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const path = require('path')
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, '/../frontend/build')))
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'))
+})
 
 module.exports = app
