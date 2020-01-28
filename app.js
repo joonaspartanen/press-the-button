c604d6e9594a4c134699964cb5015c3327a25d25
Joonas Partanen <partanen.joonas@gmail.com>
2020-01-28 18:57:49 +0200

    Changing PORT

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require('path')
 app.use(cors())
 app.use(express.static(path.join(__dirname, '/../frontend/build')))
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/../frontend/build/index.html'))
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
 module.exports = app
