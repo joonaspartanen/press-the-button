83064c373b5afc311b1662759b13f6c03b2c41e1
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-03 22:49:01 +0200

    uibuild

---
+++ backend/build/precache-manifest.72b75cacf14f02a2ab812e1ffda32a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3578f006a0067f42f2d9a2320aa6c7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afeb810d44f1914190d4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "11298bb874d98b1983c8",
+    "url": "/static/css/main.e71b5280.chunk.css"
+  },
+  {
+    "revision": "afeb810d44f1914190d4",
+    "url": "/static/js/2.de62695d.chunk.js"
+  },
+  {
+    "revision": "e528410c60e37ea33864a9f99a7c73ac",
+    "url": "/static/js/2.de62695d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11298bb874d98b1983c8",
+    "url": "/static/js/main.2cb9b8d5.chunk.js"
+  },
+  {
+    "revision": "c64dd7117f3a4cf0dbbb",
+    "url": "/static/js/runtime-main.43683077.js"
+  }
+]);
