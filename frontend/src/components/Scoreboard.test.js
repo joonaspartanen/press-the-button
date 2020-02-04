56245967562e4a6c038320b35d276c885a32baca
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-04 21:47:01 +0200

    Create test for the Scoreboard

---
+++ frontend/src/components/Scoreboard.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from '@testing-library/react'
+import Scoreboard from './Scoreboard'
+
+afterEach(cleanup)
+
+test('Scoreboard contains all players', () => {
+  const players = [
+    { name: 'Tester', score: 16 },
+    { name: 'Tester2', score: 18 },
+    { name: 'Tester3', score: 20 }
+  ]
+
+  const component = render(<Scoreboard players={players} />)
+
+  component.debug()
+
+  expect(component.container).toHaveTextContent('Tester')
+  expect(component.container).toHaveTextContent('Tester2')
+  expect(component.container).toHaveTextContent('Tester3')
+})
