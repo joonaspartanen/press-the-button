111e0b01aa19001c99e89f2de65385e16f26af84
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-05 20:44:02 +0200

    Add test for PlayAgain view

---
+++ frontend/src/components/PlayAgain.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import PlayAgain from './PlayAgain'
+
+describe('<PlayAgain />', () => {
+  let component
+  let yesButton
+  let noButton
+
+  const playAgainMock = jest.fn()
+  const leaveGameMock = jest.fn()
+
+  beforeEach(() => {
+    component = render(
+      <PlayAgain playAgain={playAgainMock} leaveGame={leaveGameMock} />
+    )
+    yesButton = component.container.querySelector('.yes-button')
+    noButton = component.container.querySelector('.no-button')
+  })
+
+  afterEach(cleanup)
+
+  test('The play again view is rendered correctly', () => {
+    expect(component.container).toHaveTextContent('You lose!')
+    expect(component.container).toHaveTextContent('Do you want to play again?')
+    expect(yesButton).toBeDefined()
+    expect(noButton).toBeDefined()
+  })
+
+  test('clicking yes button calls playAgain once', () => {
+    fireEvent.click(yesButton)
+    expect(playAgainMock).toHaveBeenCalledTimes(1)
+  })
+
+  test('clicking no button calls leaveGame once', () => {
+    fireEvent.click(noButton)
+    expect(leaveGameMock).toHaveBeenCalledTimes(1)
+  })
+})
