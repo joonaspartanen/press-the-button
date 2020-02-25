8fc8a8f9e9d33dc72400d5edc7ac921dfb616cbc
Joonas Partanen <partanen.joonas@gmail.com>
2020-02-25 21:50:59 +0200

    Adjust margins

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -3,7 +3,7 @@ import { Header } from 'semantic-ui-react'
 
 const Notification = ({ notification }) => {
   return (
-    <Header as='h3' data-cy='notification' style={{ height: '50px', marginBottom: '1em' }}>
+    <Header as='h3' data-cy='notification' style={{ height: '30px', marginBottom: '0.5em' }}>
       {notification}
     </Header>
   )
